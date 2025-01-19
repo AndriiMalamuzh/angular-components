5a8a4b4d91ff0c66538413197c8cec71b449899a
AndriiMalamuzh <andrey.malamuzh.1994@gmail.com>
2025-01-19 14:00:43 +0100

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: ["app", "stl"],
+          style: "kebab-case",
+        },
+      ],
+      // "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {
+      "prettier/prettier": "off",
+    },
+  }
+);

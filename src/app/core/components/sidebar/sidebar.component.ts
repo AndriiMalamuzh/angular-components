1ce885d177c26b4c054e6828842be74c31a0b4a2
AndriiMalamuzh <andrey.malamuzh.1994@gmail.com>
2026-03-08 16:50:43 +0100

    update angular to 20

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SidebarService } from 'src/app/core/components/sidebar/sidebar.service';
 import { MenuInterface } from 'src/app/shared/interfaces/menu.interface';
 import { MENU } from 'src/app/shared/constants/menu';
@@ -10,7 +9,6 @@ import { SidebarItemComponent } from 'src/app/core/components/sidebar/sidebar-it
 @Component({
   selector: 'app-sidebar',
   imports: [
-    CommonModule,
     SidebarHeaderComponent,
     SidebarOverlayComponent,
     SidebarItemComponent,

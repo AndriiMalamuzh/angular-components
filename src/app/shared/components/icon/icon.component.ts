1f124c42a7245bfe7e02b8d0a2b40770be4965d7
AndriiMalamuzh <andrey.malamuzh.1994@gmail.com>
2026-03-08 18:51:10 +0100

    update icons

--- src/app/shared/components/icon/icon.component.ts
+++ src/app/shared/components/icon/icon.component.ts
@@ -1,16 +1,23 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-icon',
-  imports: [],
   templateUrl: './icon.component.html',
   styleUrl: './icon.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IconComponent {
-  protected readonly environment = environment;
+  readonly name = input.required<string>();
+  readonly size = input<number>(24);
+  readonly file = input<string>('main');
 
-  name = input.required<string>();
-  size = input<number>(24);
+  readonly iconHref = computed(
+    () => `${environment.publicUrl}icons/${this.file()}.svg#${this.name()}`
+  );
 }
